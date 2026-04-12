189c49ac76d93fa784b47a957e1ce90a85f972cc
Joe <shengzhe.zhou@gmail.com>
2026-04-12 23:22:02 +1000

    setting ui:implement dark mode toggle

--- app/(drawer)/settings.tsx
+++ app/(drawer)/settings.tsx
@@ -1,11 +1,15 @@
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity, Switch} from "react-native";
 import React from "react";
-
+import { Colors } from "@/utils/Colors";
 const Settings = () => {
     
     return(
         <View style= {styles.container}>
-            <Text>Settings</Text>
+            <Text style = {styles.title}>Theme Switch</Text>
+            <TouchableOpacity style= {styles.button}onPress={() => {}}>
+                <Text>Dark Mode</Text>
+                <Switch/>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -13,7 +17,23 @@ const Settings = () => {
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        padding: 20
+        padding: 20,
+        backgroundColor: Colors.grey,
+    },
+    title : {
+        fontSize: 18,
+        fontWeight: '600',
+        marginVertical : 10,
+    },
+    button:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        backgroundColor: Colors.white,
+        padding: 15,
+        borderRadius: 10,
+        marginBottom: 10,
+
     }
 })
 
