cb5d3ff4e23e1e415ccf05a59a7c5a75dc650833
Joe <shengzhe.zhou@gmail.com>
2026-04-24 03:02:43 +1000

    feat: implement persistent theme with asyncstorage

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export type ThemeContextType = {
   currentTheme: string;
@@ -13,8 +14,25 @@ export const ThemeContext = createContext<ThemeContextType>({
 const ThemeProvider = ({ children }: { children: ReactNode }) => {
   const [theme, setTheme] = useState<string>('light');
 
+  useEffect(() => {
+    const getTheme = async() => {
+        try{
+            const savedTheme = await AsyncStorage.getItem('theme');
+
+            if(savedTheme){
+                setTheme(savedTheme);
+            }
+        }catch(error){
+            console.log('error in loading theme', error);
+        }
+
+    }
+    getTheme();
+  },[]);
+
   const toggleTheme = (newTheme: string) => {
     setTheme(newTheme);
+    AsyncStorage.setItem('theme', newTheme);
   };
 
   return (
