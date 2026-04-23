6bac7fe4fa3ee2756cf3ab33210124963a7ea7c1
Joe <shengzhe.zhou@gmail.com>
2026-04-24 01:44:12 +1000

    ui: implement color change on title

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,11 +1,26 @@
-import { Stack } from "expo-router";
-import Calculator from "@/Components/Calculator";
+import { Stack } from 'expo-router';
+import Calculator from '@/Components/Calculator';
+import { ThemeContext } from '@/context/ThemeContext';
+import { useContext } from 'react';
+import { Colors } from '@/utils/Colors';
 
 export default function Index() {
+  const { currentTheme } = useContext(ThemeContext);
   return (
     <>
-      <Stack.Screen options={{ title: "Calculator" }} />
-      <Calculator/>
+      <Stack.Screen
+        options={{
+          title: 'Calculator',
+          headerStyle: {
+            backgroundColor:
+              currentTheme === 'dark' ? Colors.dark : Colors.light,
+          },
+          headerTitleStyle: {
+            color: currentTheme === 'dark' ? Colors.white : Colors.black,
+          },
+        }}
+      />
+      <Calculator />
     </>
   );
 }
